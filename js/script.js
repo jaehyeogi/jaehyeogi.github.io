578915f4980bc5881596a5ebee3a47f7d5e102f8
jaehyeogi <61725640+jaehyeogi@users.noreply.github.com>
2020-05-08 18:20:13 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,21 @@
 var Body = {
   setColor:function(color){
     document.querySelector('body').style.color = color;
-    //     $('body').css('color',color);
   },
     // document 객체. querySelector 객체에 속해 있는 함수 >> method
   // ,필수
   setBackgroundColor: function(color){
     document.querySelector('body').style.backgroundColor = color;
-    //     $('body').css('backgroundColor',color);
-  },
-  H3Color:function(color){
-    document.querySelector('h3').style.color = color;
-//     $('body').css('color',color);
   }
 }
 var Links = {
 setColor:function(color){
   var alist = document.querySelectorAll('a');
   var i = 0;
-  while( i < alist.length){
+  while(i < alist.length){
   alist[i].style.color = color;
   i = i + 1;
    }
-  //    $('a').css('color',color);
 }
 }
 function nightdayhandler(self){
@@ -31,14 +24,12 @@ function nightdayhandler(self){
   if(actionButton.style.background === 'url("photos/moon.png")'){
   Body.setBackgroundColor('black');
   Body.setColor('white');
-  Body.H3Color('white');
   Links.setColor('white');
   actionButton.style.background = "url('photos/sun.png')";
  }
  else{
    Body.setBackgroundColor('white');
    Body.setColor('black');
-   Body.H3Color('black');
    Links.setColor('black');
    actionButton.style.background = "url('photos/moon.png')";
      }
