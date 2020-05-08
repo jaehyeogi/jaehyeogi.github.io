bc8f8dc87d6d5c6ccb1392e9bc588f2c84911489
jaehyeogi <61725640+jaehyeogi@users.noreply.github.com>
2020-05-08 14:52:13 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,37 +1,46 @@
-var Links = {
-  setColor:function(color){
-   // var target = document.querySelectorAll('a');
-   // var i =6;
-   // while(target.length){
-   //   target[i].style.color = color;
-   //   i = i + 1;
-   //     }
-   $('a').css('color',color);
-    }
-  }
 var Body = {
   setColor:function(color){
-    // document.querySelector('body').style.color = color;
-    $('body').css('color',color);
+    document.querySelector('body').style.color = color;
+    //     $('body').css('color',color);
   },
-  setBackgroundColor:function(color){
-    // document.querySelector('body').style.backgroundColor = color;
-    $('body').css('backgroundColor',color);
-},
+    // document 객체. querySelector 객체에 속해 있는 함수 >> method
+  // ,필수
+  setBackgroundColor: function(color){
+    document.querySelector('body').style.backgroundColor = color;
+    //     $('body').css('backgroundColor',color);
+  },
   H3Color:function(color){
-    // document.querySelector('h3').style.color = color;
-    $('body').css('color',color);
+    document.querySelector('h3').style.color = color;
+//     $('body').css('color',color);
   }
 }
-function nighthandler(){
+var Links = {
+setColor:function(color){
+  var alist = document.querySelectorAll('a');
+  var i = 0;
+  while(i < alist.length){
+  alist[i].style.color = color;
+  i = i + 1;
+   }
+  //    $('a').css('color',color);
+}
+}
+function nightdayhandler(self){
+  var target = document.querySelector('body');
+  var actionButton = document.getElementById('action');
+  if(actionButton.style.background === 'url("moon.png")'){
   Body.setBackgroundColor('black');
   Body.setColor('white');
   Body.H3Color('white');
   Links.setColor('white');
-        }
-function dayhandler(){
-  Body.setBackgroundColor('white');
-  Body.setColor('black') ;
-  Body.H3Color('black');
-  Links.setColor('black');
+  actionButton.style.background = "url('sun.png')";
+ }
+ else{
+   Body.setBackgroundColor('white');
+   Body.setColor('black');
+   Body.H3Color('black');
+   Links.setColor('black');
+   actionButton.style.background = "url('moon.png')";
+     }
 }
+
